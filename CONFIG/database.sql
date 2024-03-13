b56f8ec439e934778433a9d4a2d2e9075519212d
Jiří Škoda <developer@skodaj.cz>
2024-03-13 12:05:36 +0100

    Database configuration updated

--- CONFIG/database.sql
+++ CONFIG/database.sql
@@ -11,16 +11,16 @@
 
 -- ### DREG (Domain REGistry) ###
 -- Table with allowed TLDs
-CREATE TABLE DREG_TLD(
+CREATE TABLE DREG_TLDS(
     id         SERIAL  PRIMARY KEY,
-    name       TEXT    NOT NULL
+    name       TEXT    UNIQUE NOT NULL
 );
 
 -- Table with registered domains
 CREATE TABLE DREG_DOMAINS(
-    id        SERIAL   PRIMARY KEY,
+    id        SERIAL           PRIMARY KEY,
     tld       INT,
-    name      TEXT     NOT NULL,
+    name      VARCHAR(256)     NOT NULL,
 
     CONSTRAINT fk_TLD
         FOREIGN KEY(tld)
@@ -34,4 +34,64 @@ INSERT INTO DREG_TLD (name) VALUES ('eu');
 INSERT INTO DREG_TLD (name) VALUES ('net');
 INSERT INTO DREG_TLD (name) VALUES ('org');
 
+
+
+
+-- ### APPLICATION ###
+
+-- Table with addresses
+CREATE TABLE ADDRESSES(
+    id            SERIAL        PRIMARY KEY,
+    street        VARCHAR(128)  NOT NULL,
+    city          VARCHAR(128)  NOT NULL,
+    province      VARCHAR(128)  NOT NULL,
+    country_code  CHAR(2)       NOT NULL,
+);
+
+-- Table with clients (a.k.a. personal data of users)
+CREATE TABLE CLIENTS(
+    id            SERIAL            PRIMARY KEY,
+    name          VARCHAR(128)      NOT NULL,
+    surname       VARCHAR(128)      NOT NULL,
+    email         VARCHAR(384)      NOT NULL,
+    address       INT               NOT NULL,
+    company       VARCHAR(128)      NOT NULL,
+    company_unit  VARCHAR(128)      NOT NULL,
+
+    CONSTRAINT fk_address
+        FOREIGN KEY (address)
+            REFERENCES ADDRESSES(id)
+);
+
+-- Table with users of system
+CREATE TABLE USERS(
+    id           SERIAL           PRIMARY KEY,
+    password     VARCHAR(1024)    NOT NULL,
+    client       INT              UNIQUE        NOT NULL,
+
+    CONSTRAINT fk_client
+        FOREIGN KEY (client)
+            REFERENCES CLIENTS(id)
+);
+
+-- Table with hostings
+CREATE TABLE HOSTINGS(
+    id           SERIAL           PRIMARY KEY,
+    domain       INT              NOT NULL,
+    disk         INT              NOT NULL,
+    database     INT              NOT NULL,
+    
+);
+
+
+-- Table mapping users to its owned hostings
+CREATE TABLE USER_HOSTINGS(
+    id          SERIAL            PRIMARY KEY,
+    user        INT               NOT NULL,
+    hosting     INT               NOT NULL,
+
+    CONSTRAINT fk_user
+        FOREIGN KEY (user)
+            REFERENCES USERS(id)
+);
 
